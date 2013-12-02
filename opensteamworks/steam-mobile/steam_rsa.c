50da1bfd4ba72e37ad20b79b5d607937a92ae37d
Tobias Tangemann <tobiastangemann@gmail.com>
2013-12-02 13:46:52 +0100

    Small code fixes

--- opensteamworks/steam-mobile/steam_rsa.c
+++ opensteamworks/steam-mobile/steam_rsa.c
@@ -1,21 +1,21 @@
 /*
-https://steamcommunity.com/mobilelogin/getrsakey?username=<steamusername>
-{"success":true,"publickey_mod":"pubkeyhex","publickey_exp":"pubkeyhex","timestamp":"165685150000"}
+ https://steamcommunity.com/mobilelogin/getrsakey?username=<steamusername>
+ {"success":true,"publickey_mod":"pubkeyhex","publickey_exp":"pubkeyhex","timestamp":"165685150000"}
+ 
+ 
+ https://steamcommunity.com/mobilelogin/dologin/
+ 
+ password=<base64rsaencryptedpwd>&username=<steamusername>&emailauth=&captchagid=-1&captcha_text=&emailsteamid=&rsatimestamp=165685150000&remember_login=true&donotcache=1368831657863
+ 
+ */
 
 
-https://steamcommunity.com/mobilelogin/dologin/
-
-password=<base64rsaencryptedpwd>&username=<steamusername>&emailauth=&captchagid=-1&captcha_text=&emailsteamid=&rsatimestamp=165685150000&remember_login=true&donotcache=1368831657863
-
-*/
-
-
-#if ! defined OPENSSL_CRYPTO && ! defined NSS_CRYPTO
+#if !defined OPENSSL_CRYPTO && !defined NSS_CRYPTO
 #ifdef __APPLE__
-#define OPENSSL_CRYPTO 0
+#define OPENSSL_CRYPTO
 #warning Defaulting to OpenSSL for RSA encryption
 #else /* __APPLE__ */
-#define NSS_CRYPTO 0
+#define NSS_CRYPTO
 #warning Defaulting to NSS for RSA encryption
 #endif /* __APPLE__ */
 #endif /* ! defined OPENSSL_CRYPTO && ! defined NSS_CRYPTO */
@@ -49,9 +49,9 @@ steam_encrypt_password_openssl(const gchar *modulus_str, const gchar *exponent_s
 	gchar *output = NULL;
 	guchar *encrypted;
 	int rv;
-
+  
 	ERR_load_crypto_strings();
-
+  
 	bn_modulus = BN_new();
 	rv = BN_hex2bn(&bn_modulus, modulus_str);
 	if (rv == 0)
@@ -60,7 +60,7 @@ steam_encrypt_password_openssl(const gchar *modulus_str, const gchar *exponent_s
 		BN_free(bn_modulus);
 		return NULL;
 	}
-
+  
 	bn_exponent = BN_new();
 	rv = BN_hex2bn(&bn_exponent, exponent_str);
 	if (rv == 0)
@@ -70,7 +70,7 @@ steam_encrypt_password_openssl(const gchar *modulus_str, const gchar *exponent_s
 		BN_clear_free(bn_exponent);
 		return NULL;
 	}
-
+  
 	rsa = RSA_new();
 	if (rsa == NULL)
 	{
@@ -83,13 +83,13 @@ steam_encrypt_password_openssl(const gchar *modulus_str, const gchar *exponent_s
 	rsa->n = bn_modulus;
 	BN_free(rsa->e);
 	rsa->e = bn_exponent;
-
+  
 	encrypted = g_new0(guchar, RSA_size(rsa));
-	rv = RSA_public_encrypt((int)(strlen(password) - 1),
-							(const unsigned char *)password,
-							encrypted,
-							rsa,
-							RSA_PKCS1_PADDING);
+	rv = RSA_public_encrypt((int)(strlen(password)),
+                          (const unsigned char *)password,
+                          encrypted,
+                          rsa,
+                          RSA_PKCS1_PADDING);
 	if (rv < 0)
 	{
 		unsigned long error_num = ERR_get_error();
@@ -99,7 +99,7 @@ steam_encrypt_password_openssl(const gchar *modulus_str, const gchar *exponent_s
 		g_free(encrypted);
 		return NULL;
 	}
-
+  
 	/*
 	 BIO *b64_filter = BIO_new(BIO_f_base64());
 	 BIO *mem_bio = BIO_new(BIO_s_mem());
@@ -114,12 +114,12 @@ steam_encrypt_password_openssl(const gchar *modulus_str, const gchar *exponent_s
 	 BIO_free_all(mem_bio);
 	 */
 	output = purple_base64_encode(encrypted, RSA_size(rsa));
-
+  
 	// Cleanup
 	RSA_free(rsa);
 	ERR_free_strings();
 	g_free(encrypted);
-
+  
 	return output;
 }
 #endif
@@ -184,7 +184,7 @@ steam_encrypt_password_nss(const gchar *modulus_str, const gchar *exponent_str, 
 		SECItem m_exponent;
 	} inPubKey;
 	SECStatus rv;
-
+  
 	const SEC_ASN1Template MyRSAPublicKeyTemplate[] = {
 		{ SEC_ASN1_SEQUENCE, 0, NULL, sizeof(struct MyRSAPublicKey) },
 		{ SEC_ASN1_INTEGER, offsetof(struct MyRSAPublicKey, m_modulus), },
@@ -193,15 +193,15 @@ steam_encrypt_password_nss(const gchar *modulus_str, const gchar *exponent_str, 
 	};
 	
 	temp = hexstring_to_binary(modulus_str);
-	inPubKey.m_modulus.data = (unsigned char *) PORT_Alloc(modlen + 10); 
-	memcpy(inPubKey.m_modulus.data, temp, modlen); 
-	inPubKey.m_modulus.len = modlen; 
-	inPubKey.m_modulus.type = siUnsignedInteger; 
+	inPubKey.m_modulus.data = (unsigned char *) PORT_Alloc(modlen + 10);
+	memcpy(inPubKey.m_modulus.data, temp, modlen);
+	inPubKey.m_modulus.len = modlen;
+	inPubKey.m_modulus.type = siUnsignedInteger;
 	g_free(temp);
 	
 	temp = hexstring_to_binary(exponent_str);
-	inPubKey.m_exponent.data = (unsigned char *) PORT_Alloc(explen + 10); 
-	memcpy(inPubKey.m_exponent.data, temp, explen); 
+	inPubKey.m_exponent.data = (unsigned char *) PORT_Alloc(explen + 10);
+	memcpy(inPubKey.m_exponent.data, temp, explen);
 	inPubKey.m_exponent.len = explen;
 	inPubKey.m_exponent.type = siUnsignedInteger;
 	g_free(temp);
@@ -227,8 +227,8 @@ steam_encrypt_password_nss(const gchar *modulus_str, const gchar *exponent_str, 
 	}
 	
 	/*tmpstr = BTOA_DataToAscii(encrypted, modlen);
-	output = g_strdup(tmpstr);
-	PORT_Free(tmpstr);*/
+   output = g_strdup(tmpstr);
+   PORT_Free(tmpstr);*/
 	output = purple_base64_encode(encrypted, modlen);
 	
 	g_free(encrypted);
@@ -244,10 +244,9 @@ steam_encrypt_password(const gchar *modulus_str, const gchar *exponent_str, cons
 {
 #ifdef NSS_CRYPTO
 	return steam_encrypt_password_nss(modulus_str, exponent_str, password);
-#elif OPENSSL_CRYPTO
+#elif defined OPENSSL_CRYPTO
 	return steam_encrypt_password_openssl(modulus_str, exponent_str, password);
 #else
-	return "";
+	return g_new0(gchar, 1);
 #endif
-
 }
